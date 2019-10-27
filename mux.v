3e6d661fc48897795f92b3db5fc36c0e4152fcdc
PrajwalShenoy <30391965+PrajwalShenoy@users.noreply.github.com>
2019-10-27 16:07:47 +0530

    Update mux.v

--- mux.v
+++ mux.v
@@ -5,8 +5,8 @@ input sel, a, b;
 output out;
 wire w;
 notg not_gate(w, sel);
-pmos p1(out, a, w);
-nmos n1(out, a, sel);
-pmos p2(out, b, sel);
-nmos n2(out, b, w);
+pmos p1(out, b, w);
+nmos n1(out, b, sel);
+pmos p2(out, a, sel);
+nmos n2(out, a, w);
 endmodule
