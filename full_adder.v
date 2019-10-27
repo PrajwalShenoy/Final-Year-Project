add23cbfa709d86caf6da94de1653b52f51ccfda
sanath11598 <54355389+sanath11598@users.noreply.github.com>
2019-10-27 18:36:50 +0530

    Create full_adder.v

---
+++ full_adder.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.10.2019 17:32:17
+// Design Name: 
+// Module Name: andg_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module full_adder(sum,carry.a,b,cin);
+	input a,b,cin;
+    output sum,carry;
+    wire x,y,x1;
+    
+    half_adder hadd(x,y,a,b); //x = a^b y= a.b 
+    half_adder hadd1(sum,x1,x,cin);// sum =x^cin x1=x. cin
+    org        or1(carry,y,x1);
+    
+endmodule
