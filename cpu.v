47f764185a6a1445f1acb349189b5e653248a650
Prajwal Shenoy <prajwalkpshenoy@gmail.com>
2020-02-22 01:53:24 +0530

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -10,12 +10,17 @@ module cpu(outM,PCout,WriteM,addrM,instruction,inM,rst,clk);
     output WriteM;
     
     wire zr,ng;
-    wire [15:0]dout,aout,mux2,mux1,inst1;
+    wire [15:0]dout,aout,mux2,mux1,inst1,alu_out;
     wire Asel,load;
+    wire vdd;
+    
+    assign vdd = 1'b1;
     
     assign WriteM = inst1[3];
     assign addrM = aout;
-    alu a_lu(outM, zr, ng, dout, mux2, inst1[11:10], inst1[9:8], inst1[7:6]);
+    neg_register16 alu_reg(outM, clk, vdd, alu_out);
+//  module neg_register16(out, clk, load, in);
+    alu a_lu(alu_out, zr, ng, dout, mux2, inst1[11:10], inst1[9:8], inst1[7:6]);
     d_reg d1(dout, clk, inst1[4], outM);
     a_reg a1(aout, clk, Asel, mux1);
     decoder dec1(inst1, Asel, load, instruction, zr, ng);
